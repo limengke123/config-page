1b644626d64d39a5a13bfba46b45f85823937569
limengke <752140235@qq.com>
2018-07-27 17:15:31 +0800

    对images文件过滤git

--- server/controller.js
+++ server/controller.js
@@ -7,7 +7,7 @@ const {getConfigData, setConfigData} = require('./util')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images')
+        cb(null, './images')
     },
     filename: function (req, file, cb) {
         const fileFormat = (file.originalname).split('.')
